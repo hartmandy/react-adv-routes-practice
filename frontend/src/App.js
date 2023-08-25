538bee6a83000bde07654b776e3381d3cdcbb61c
hartmandy <120344775+hartmandy@users.noreply.github.com>
2023-08-25 15:25:11 -0400

    added newsletter and useFetcher

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,9 @@ import RootLayout from "./routes/Root";
 import EventsRoot from "./routes/EventsRoot";
 import ErrorPage from "./routes/Error";
 import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, {
+  action as newsletterAction,
+} from "./routes/Newsletter";
 
 const router = createBrowserRouter([
   {
@@ -47,6 +50,11 @@ const router = createBrowserRouter([
             action: manipulateEventAction,
           },
         ],
+      },
+      {
+        path: "newsletter",
+        element: <NewsletterPage />,
+        action: newsletterAction,
       },
     ],
   },
